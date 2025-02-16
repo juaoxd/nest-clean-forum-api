0af43ba25d3ced410defb1356cefa5a228a8647a
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-02-16 18:22:33 -0300

    add vitest configuration and update import paths for consistency

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -5,9 +5,9 @@ import {
   UnauthorizedException,
   UsePipes,
 } from '@nestjs/common'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcrypt'
 
