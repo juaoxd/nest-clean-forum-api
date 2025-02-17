8021f82328934c3f30c3ba76239bf56836c5013e
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-02-17 20:29:09 -0300

    refactor: update ESLint rules and improve type safety in domain events and use cases

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -13,7 +13,7 @@ export abstract class Entity<Props> {
     this._id = id ?? new UniqueEntityID(id)
   }
 
-  public equals(entity: Entity<any>) {
+  public equals(entity: Entity<unknown>) {
     if (entity === this) {
       return true
     }
--- src/core/events/domain-events.spec.ts
+++ src/core/events/domain-events.spec.ts
@@ -3,6 +3,16 @@ import { UniqueEntityID } from '../entities/unique-entity-id'
 import { DomainEvent } from './domain-event'
 import { DomainEvents } from './domain-events'
 
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
 class CustomAggregateCreated implements DomainEvent {
   public occurredAt: Date
   private aggregate: CustomAggregate
@@ -14,16 +24,6 @@ class CustomAggregateCreated implements DomainEvent {
 
   public getAggregateId(): UniqueEntityID {
     return this.aggregate.id
-  }
-}
-
-class CustomAggregate extends AggregateRoot<null> {
-  static create() {
-    const aggregate = new CustomAggregate(null)
-
-    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
-
-    return aggregate
   }
 }
 
