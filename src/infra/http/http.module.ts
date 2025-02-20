ad833b9e44548a4dd4e1f660aa71e50c9d3d865b
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-02-20 00:26:23 -0300

    feat: refactor account creation to use RegisterStudentUseCase and remove direct database access

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -8,6 +8,7 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -21,6 +22,7 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     AuthenticateStudentUseCase,
+    RegisterStudentUseCase,
   ],
 })
 export class HttpModule {}
