3ff321eac1f20195548b094c2ecbabb2d1351239
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-02-28 17:36:13 -0300

    feat: add support for attachments in edit question functionality, including schema updates and integration with factories

--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -15,6 +15,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 })
 
 type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
@@ -32,7 +33,7 @@ export class EditQuestionController {
     @Param('id') questionId: string,
     @CurrentUser() user: UserPayload,
   ) {
-    const { title, content } = body
+    const { title, content, attachments } = body
     const userId = user.sub
 
     const result = await this.editQuestion.execute({
@@ -40,7 +41,7 @@ export class EditQuestionController {
       content,
       questionId,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
     })
 
     if (result.isLeft()) {
